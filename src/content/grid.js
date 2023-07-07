407d113efd047c44bfdb3b71031919ac4e4de71a
Jack Gitter <jackgitter@Jack-Gitters-MacBook-Pro.local>
2023-07-07 13:41:38 -0400

    yuh

--- src/content/grid.js
+++ src/content/grid.js
@@ -9,19 +9,28 @@ function Grid() {
   return (
       <div>
             <div className="row row-height justify-content-center">
-                <div className="col col-lg-4 p-0"><button className="select-button">?</button></div>
-                <div className="col col-lg-4 p-0"><button className="select-button">?</button></div>
-                <div className="col col-lg-4 p-0"><button className="select-button">?</button></div>
+                <div className="col col-lg-3 p-0"></div>
+                <div className="col col-lg-3 p-0"><img className='team-picture' src='https://picsum.photos/20'></img></div>
+                <div className="col col-lg-3 p-0"><img className='team-picture' src='https://picsum.photos/20'></img></div>
+                <div className="col col-lg-3 p-0"><img className='team-picture' src='https://picsum.photos/20'></img></div>
             </div>
             <div className="row row-height justify-content-center">
-                <div className="col col-lg-4 p-0"><button className="select-button">?</button></div>
-                <div className="col col-lg-4 p-0"><button className="select-button">?</button></div>
-                <div className="col col-lg-4 p-0"><button className="select-button">?</button></div>
+                <div className="col col-lg-3 p-0"><img className='team-picture' src='https://picsum.photos/20'></img></div>
+                <div className="col col-lg-3 p-0"><button className="select-button">?</button></div>
+                <div className="col col-lg-3 p-0"><button className="select-button">?</button></div>
+                <div className="col col-lg-3 p-0"><button className="select-button">?</button></div>
             </div>
             <div className="row row-height justify-content-center">
-                <div className="col col-lg-4 p-0"><button className="select-button">?</button></div>
-                <div className="col col-lg-4 p-0"><button className="select-button">?</button></div>
-                <div className="col col-lg-4 p-0"><button className="select-button">?</button></div>
+                <div className="col col-lg-3 p-0"><img className='team-picture' src='https://picsum.photos/20'></img></div>
+                <div className="col col-lg-3 p-0"><button className="select-button">?</button></div>
+                <div className="col col-lg-3 p-0"><button className="select-button">?</button></div>
+                <div className="col col-lg-3 p-0"><button className="select-button">?</button></div>
+            </div>
+            <div className="row row-height justify-content-center">
+                <div className="col col-lg-3 p-0"><img className='team-picture' src='https://picsum.photos/20'></img></div>
+                <div className="col col-lg-3 p-0"><button className="select-button">?</button></div>
+                <div className="col col-lg-3 p-0"><button className="select-button">?</button></div>
+                <div className="col col-lg-3 p-0"><button className="select-button">?</button></div>
             </div>
       </div>
   );
